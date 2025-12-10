2555c1be2ebadab3b25cb8f6bd2d2f9f1cfef525
EdoDiT <edoardo.di.tommaso.99@gmail.com>
2025-12-10 19:44:43 +0000

    resulting images and data

---
+++ LayeredVectorization/run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_beach_vendor.jpg --file_save_name beach_vendor
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_berengo_coffee.jpg --file_save_name berengo_coffee
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_berengo_cruise.jpg --file_save_name berengo_cruise
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_ghirri_lake.jpg --file_save_name ghirri_lake
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_ghirri_swing.jpg --file_save_name ghirri_swing
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_hiker.jpg --file_save_name hiker
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_mountain_house.jpg --file_save_name mountain_house
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_nizar.jpg --file_save_name nizar
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_sunbathing.jpg --file_save_name sunbathing
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_valley.jpg --file_save_name valley
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_wide_valley.jpg --file_save_name wide_valley
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_berengo_asylum.jpg --file_save_name berengo_asylum
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_berengo_couple.jpg --file_save_name berengo_couple
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_berengo_road.jpg --file_save_name berengo_road
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_ghirri_slide.jpg --file_save_name ghirri_slide
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_gramps.jpg --file_save_name gramps
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_lake_house.jpg --file_save_name lake_house
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_mountain.jpg --file_save_name mountain
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_still_life.jpg --file_save_name still_life
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_tree.jpg --file_save_name tree
+python main.py --config config/base_config.yaml --target_image target_imgs/resized_vietnam.jpg --file_save_name vietnam
